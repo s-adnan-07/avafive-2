56c237b8592a6415192e3f3a24e800e14e96a955
Adnan <dev.adnan@hotmail.com>
2024-06-01 21:09:10 +0400

    Converted to monorepo mode

--- apps/api-gateway/src/app.controller.spec.ts
+++ apps/api-gateway/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('should return "App: Hello World!"', () => {
+      expect(appController.getHello()).toBe('App: Hello World!')
     })
   })
 })
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class AppService {
+export class UsersService {
   getHello(): string {
-    return 'Hello World!'
+    return 'Users: Hello World!'
   }
 }
